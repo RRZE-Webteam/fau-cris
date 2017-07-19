7b07004e627af491cb881dbfc2e6064f186b6613
cassandre <info@barbara-bothe.de>
2017-07-19 08:55:46 +0200

    CSS einbinden wenn nötig (= Shortcode  auf Seite vorhanden)

--- fau-cris.php
+++ fau-cris.php
@@ -882,9 +882,9 @@ class FAU_CRIS {
     public static function cris_enqueue_styles() {
         global $post;
         $plugin_url = plugin_dir_url(__FILE__);
-        wp_enqueue_style( 'cris', $plugin_url . 'css/cris.css' );
-        if ($post && has_shortcode($post->post_content, 'cris')) {
-            //wp_enqueue_style('cris', $plugin_url . 'css/cris.css');
+        if ($post && has_shortcode($post->post_content, 'cris') 
+                || $post && has_shortcode($post->post_content, 'cris-custom')) {
+            wp_enqueue_style('cris', $plugin_url . 'css/cris.css');
             wp_enqueue_script('cris', $plugin_url . 'js/cris.js', array ( 'jquery' ));
         }
     }
