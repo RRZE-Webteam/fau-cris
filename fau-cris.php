e2fc74e084377e665b8d4569f4913edb21b047fc
Cassandre <barbara.bothe@fau.de>
2022-01-10 14:45:27 +0100

    Versionsnummer

--- fau-cris.php
+++ fau-cris.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: FAU CRIS
  * Description: Anzeige von Daten aus dem FAU-Forschungsportal CRIS in WP-Seiten
- * Version: 3.17.4
+ * Version: 3.18.0
  * Author: RRZE-Webteam
  * Author URI: http://blogs.fau.de/webworking/
  * Text Domain: fau-cris
