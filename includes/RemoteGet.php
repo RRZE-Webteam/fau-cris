2ad6f28473caf9a1013059b878fbf80a849dda29
rvdforst <rolf.v.d.forst@fau.de>
2023-08-23 16:53:22 +0200

    Update RemoteGet.php

--- includes/RemoteGet.php
+++ includes/RemoteGet.php
@@ -12,41 +12,42 @@ class RemoteGet
         'method' => 'GET'
     ];
 
-    public static function retrieveResponse(string $url, array $args = [], int $code = 200, bool $safe = true)
+    public static function retrieveContent(string $url, array $args = [], int $code = 200, bool $safe = true)
     {
         $args = wp_parse_args($args, self::$defaultArgs);
-        $args = array_intersect_key($args, $args);
+        $args = array_intersect_key($args, self::$defaultArgs);
 
+        $content = Cache::get($url);
+        if ($content === false) {
+            $response = self::remoteGet($url, $args, $safe);
+            if (is_wp_error($response)) {
+                do_action(
+                    'rrze.log.error',
+                    'Plugin: {plugin} WP-Error: {wp-error}',
+                    [
+                        'plugin' => 'fau-cris',
+                        'wp-error' => $response->get_error_message(),
+                        'method' => debug_backtrace()[1]['function'] ?? '',
+                        'url' => $url
+                    ]
+                );
+                $response = [];
+            } elseif (wp_remote_retrieve_response_code($response) != $code) {
+                $response = [];
+            }
+            $content = $response['body'] ?? '';
+            Cache::set($url, $content);
+        }
+
+        return $content;
+    }
+
+    private static function remoteGet(string $url, array $args, bool $safe)
+    {
         if ($safe) {
-            $response = self::getSafeResponse($url, $args);
+            return wp_safe_remote_get($url, $args);
         } else {
-            $response = self::getResponse($url, $args);
+            return wp_remote_get($url, $args);
         }
-        if (is_wp_error($response)) {
-            do_action(
-                'rrze.log.error',
-                'Plugin: {plugin} WP-Error: {wp-error}',
-                [
-                    'plugin' => 'fau-cris',
-                    'wp-error' => $response->get_error_message(),
-                    'method' => debug_backtrace()[1]['function'] ?? '',
-                    'url' => $url
-                ]
-            );
-        } elseif (wp_remote_retrieve_response_code($response) != $code) {
-            $response = false;
-        }
-
-        return $response;
-    }
-
-    private static function getSafeResponse(string $url, array $args)
-    {
-        return wp_safe_remote_get($url, $args);
-    }
-
-    private static function getResponse(string $url, array $args)
-    {
-        return wp_remote_get($url, $args);
     }
 }
