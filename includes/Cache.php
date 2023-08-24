44ebad0fd88a8ca1bacd87153445cdb28efba8bb
rvdforst <rolf.v.d.forst@fau.de>
2023-08-24 10:16:36 +0200

    Update Cache.php

--- includes/Cache.php
+++ includes/Cache.php
@@ -6,17 +6,14 @@ defined('ABSPATH') || exit;
 
 class Cache
 {
+    private static $ttl = 6 * HOUR_IN_SECONDS;
+
     public static function set(string $url, string $ical)
     {
         $prefix = parse_url($url, PHP_URL_SCHEME);
         $key = (strpos($url, $prefix) === 0) ? substr($url, strlen($prefix)) : $url;
         $cacheOption = 'cris_' . md5($key);
-        $ttl = HOUR_IN_SECONDS;
-        if (is_multisite()) {
-            set_site_transient($cacheOption, $ical, $ttl);
-        } else {
-            set_transient($cacheOption, $ical, $ttl);
-        }
+        set_transient($cacheOption, $ical, self::$ttl);
     }
 
     public static function get(string $url)
@@ -24,12 +21,7 @@ class Cache
         $prefix = parse_url($url, PHP_URL_SCHEME);
         $key = (strpos($url, $prefix) === 0) ? substr($url, strlen($prefix)) : $url;
         $cacheOption = 'cris_' . md5($key);
-        if (is_multisite()) {
-            $ical = get_site_transient($cacheOption);
-        } else {
-            $ical = get_transient($cacheOption);
-        }
-        return $ical;
+        return get_transient($cacheOption);
     }
 
     public static function delete(string $url): bool
@@ -37,11 +29,6 @@ class Cache
         $prefix = parse_url($url, PHP_URL_SCHEME);
         $key = (strpos($url, $prefix) === 0) ? substr($url, strlen($prefix)) : $url;
         $cacheOption = 'cris_' . md5($key);
-        if (is_multisite()) {
-            $return = delete_site_transient($cacheOption);
-        } else {
-            $return = delete_transient($cacheOption);
-        }
-        return $return;
+        return delete_transient($cacheOption);
     }
 }
